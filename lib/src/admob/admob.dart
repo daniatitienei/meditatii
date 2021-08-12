872adbae0b750e8201dce5096207098d7b145241
daniatitienei <xgamerhd52@gmail.com>
2021-08-12 12:04:57 +0300

    1.0.1

--- lib/src/admob/admob.dart
+++ lib/src/admob/admob.dart
@@ -1,9 +1,9 @@
 class AdMob {
   // NOTE - Sunt alea de test
-  // final String bannerAdId = 'ca-app-pub-3940256099942544/6300978111';
-  // final String interstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+  final String bannerAdId = 'ca-app-pub-3940256099942544/6300978111';
+  final String interstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
 
   // REVIEW - Sunt alea cu bani
-  final String bannerAdId = 'ca-app-pub-3178535762944426/2629576036';
-  final String interstitialAdId = 'ca-app-pub-3178535762944426/1367437587';
+  // final String bannerAdId = 'ca-app-pub-3178535762944426/2629576036';
+  // final String interstitialAdId = 'ca-app-pub-3178535762944426/1367437587';
 }
